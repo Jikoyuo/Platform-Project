b52d1c7db7da6d4b3f7692924fcdd0881defcd8c
Jikoyuo <chornaeld@gmail.com>
2024-05-22 20:36:09 +0700

    commit 1

---
+++ project/script.js
@@ -0,0 +1,24 @@
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+const login1Btn = document.getElementById('pindah');
+
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
+
+document.getElementById("login").addEventListener("click", function() {
+    container.classList.remove("active");
+    window.open("https://github.com/Jikoyuo", '_blank');
+    // your event handling code
+    alert("Button Clicked!");
+});
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
+});
+
+login1Btn.addEventListener('click', () => {
+    container.classList.remove("active");
+});
