f7b1f4d25a9de1bc1728bbaa6563ec858b659d5e
HearTh13 <gun123sokero@gmail.com>
2024-06-07 18:36:05 +0700

    Changes to be committed: modified:   resources/views/index.blade.php modified:   resources/views/layouts/main.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -117,10 +117,4 @@
 </div>
     <!-- end katalog penjualan -->
 
-
-
-    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
-    <script
-        src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
-    <script
-        src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.2/js/bootstrap.min.js"></script>
+@endsection
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -35,5 +35,10 @@
     </head>
     <body>
         @yield('content')
+        <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+        <script
+            src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+        <script
+            src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.2/js/bootstrap.min.js"></script>
     </body>
 </html>
