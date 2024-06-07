5ff1ade640b1298bbb5f7f7929ace619b9f71472
HearTh13 <gun123sokero@gmail.com>
2024-06-07 18:47:29 +0700

    modified:   resources/views/partials/catalog.blade.php

--- resources/views/partials/catalog.blade.php
+++ resources/views/partials/catalog.blade.php
@@ -2,12 +2,12 @@
 <div class="row" id="movie-list">
     <div class="col-md-3">
         <div class="card">
-            <!-- <img src="no_image.webp" class="card-img-top" alt="{{$product->name}}" style="max-height:360px; min-height:360px"> -->
+            <!-- <img src="no_image.webp" class="card-img-top" alt="$product->name" style="max-height:360px; min-height:360px"> -->
             <img src="no_image.webp" class="card-img-top" alt="Title" style="max-height:360px; min-height:360px">
             <div class="card-body">
-                <!-- <h5 class="card-title">{{$product->name}}</h5> -->
+                <!-- <h5 class="card-title">$product->name</h5> -->
                 <h5 class="card-title">name</h5>
-                <!-- <h6 class="card-subtitle mb-2 text-body-secondary">{{$product->year}}</h6> -->
+                <!-- <h6 class="card-subtitle mb-2 text-body-secondary">$product->year</h6> -->
                 <h6 class="card-subtitle mb-2 text-body-secondary">year</h6>
                 <a href="#" class="card-link see-detail" data-bs-toggle="modal" data-bs-target="#exampleModal" data-id="">See Detail</a>
             </div>
