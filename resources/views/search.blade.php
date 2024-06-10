d4c92b1d2226b70585d19cbda97da4e4f048dd37
HearTh13 <gun123sokero@gmail.com>
2024-06-10 15:06:37 +0700

    new file:   resources/css/style.css 	modified:   resources/views/search.blade.php

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -3,13 +3,12 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Netflix</title>
         <link
             href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
             rel="stylesheet"
             integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
             crossorigin="anonymous">
-        <link rel="stylesheet" href="styleSearch.css">
+        <link rel="stylesheet" href="style.css">
     </head>
     <body>
 
