be64a28e6d63ceae4d1d4cd69cf91feff71d565f
HearTh13 <gun123sokero@gmail.com>
2024-06-10 11:37:43 +0700

    modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,33 @@
 use App\Http\Controllers\DBCategoryController;
 use App\Http\Controllers\DBProductController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RedirectController;
+use App\Models\DBCategory;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [RedirectController::class, 'redirectToHome']);
+
+Route::get('/home', [DBProductController::class, 'index']);
+
+Route::get('/desc', function () {
+    return view('desc');
 });
+
+Route::get('/admin', function () {
+    return view('admin');
+});
+
+// Tambahkan nama rute login
+Route::get('/login', function () {
+    return view('login');
+});
+
+// Tambahkan nama rute register
+Route::get('/register', function () {
+    return view('register');
+});
+
+Route::get('/genres', [DBCategoryController::class, 'index']);
+
+Route::get('/genres/{genre:slug}', [DBCategoryController::class, 'index']);
+
+Route::get('/search', [DBProductController::class, 'index']);
