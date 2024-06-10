1842d73fdf0473250dac1031fbda0eb2287ad750
Jikoyuo <chornaeld@gmail.com>
2024-06-10 14:34:58 +0700

    styling

--- resources/views/desc.blade.php
+++ resources/views/desc.blade.php
@@ -16,7 +16,7 @@
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <section id="section1">
-
+    <div id="particles-js" class="particles"></div>
         @include('partials.navbarDesc')
         <div class="img-container">
             <div class="bg-image" style="background-image: url('Dune.jpeg');"></div>
