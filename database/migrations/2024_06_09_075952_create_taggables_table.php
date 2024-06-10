7b76d8a88afa4db8212a2ec9314ed0cf16a0cbec
HearTh13 <gun123sokero@gmail.com>
2024-06-10 11:30:23 +0700

    new file:   app/Models/Taggables.php new file:   database/migrations/2024_06_09_075952_create_taggables_table.php

---
+++ database/migrations/2024_06_09_075952_create_taggables_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('taggables', function (Blueprint $table) {
+            $table->integer('tag_id');
+            $table->integer('taggable_id');
+            $table->string('taggable_type')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('taggables');
+    }
+};
