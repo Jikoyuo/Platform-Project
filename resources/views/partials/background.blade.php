5343bb812571b56b55d0fa972adb3381f198f4c2
StefanusSaputra4 <stefanus.usd@gmail.com>
2024-06-10 16:26:39 +0700

    modified:   resources/views/genres.blade.php 	modified:   resources/views/partials/background.blade.php 	modified:   resources/views/search.blade.php

--- resources/views/partials/background.blade.php
+++ resources/views/partials/background.blade.php
@@ -1,8 +1,8 @@
  <!-- gambar background -->
  <div class="img-container">
-    <div class="bg-image" style="background-image: url('Dune.jpeg');"></div>
-    <div class="bg-image" style="background-image: url('Dunkirk.jpeg');"></div>
-    <div class="bg-image" style="background-image: url('PeakyBlinders.jpeg');"></div>
-    <div class="bg-image" style="background-image: url('endgame.jpeg');"></div>
+    <div class="bg-image" style="background-image: url('../Dune.jpeg');"></div>
+    <div class="bg-image" style="background-image: url('../Dunkirk.jpeg');"></div>
+    <div class="bg-image" style="background-image: url('../PeakyBlinders.jpeg');"></div>
+    <div class="bg-image" style="background-image: url('../endgame.jpeg');"></div>
 </div>
 <!-- end gambar background -->
