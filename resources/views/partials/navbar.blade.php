1790d21ffb3d8e5d086dfc0baf4086e100ab2b5a
HearTh13 <gun123sokero@gmail.com>
2024-06-11 17:45:29 +0700

    modified:   app/Http/Controllers/DBProductController.php 	modified:   app/Http/Controllers/DBUsersController.php 	modified:   resources/views/layouts/main.blade.php 	modified:   resources/views/partials/navbar.blade.php 	modified:   routes/web.php

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -4,35 +4,29 @@
     <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
     <nav class="navbar navbar-expand-lg mt-5">
         <div class="container-fluid">
-            <a class="navbar-brand text-danger" href="#">Kasetflix</a>
+            <a class="navbar-brand text-danger" href="/">Kasetflix</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNavDropdown">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link active text-white" aria-current="page" href="#">Home</a>
+                        <a class="nav-link active text-white" aria-current="page" href="/">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link text-white" href="horror.html">Horror</a>
+                        <a class="nav-link text-white" href="/genres/horror">Horror</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link text-white" href="#">Sci-Fi</a>
+                        <a class="nav-link text-light" href="/genres">Genres</a>
                     </li>
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            More
+                            More Genres
                         </a>
                         <ul class="dropdown-menu multi-column">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Comedy</a></li>
-                            <li><a class="dropdown-item" href="#">Romance</a></li>
-                            <li><a class="dropdown-item" href="#">Fantasy</a></li>
-                            <li><a class="dropdown-item" href="#">Documentary</a></li>
-                            <li><a class="dropdown-item" href="#">Thriller</a></li>
-                            <li><a class="dropdown-item" href="#">Anime</a></li>
-                            <li><a class="dropdown-item" href="#">Drama</a></li>
-                            <li><a class="dropdown-item" href="#">Fiction</a></li>
+                            @foreach ($genres as $genre)
+                                <li><a class="dropdown-item" href="/genres/{{$genre->slug}}">{{$genre->genre}}</a></li>
+                            @endforeach
                         </ul>
                     </li>
                 </ul>
@@ -49,7 +43,7 @@
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item dropdown">
                         <a class="nav-link" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            <img src="profile.jpeg" width="40" height="40" class="rounded-circle">
+                            <img src="../profile.jpeg" width="40" height="40" class="rounded-circle">
                         </a>
                         <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownMenuLink">
                             <li><a class="dropdown-item" href="admin.html">Dashboard</a></li>
@@ -63,7 +57,6 @@
             </div>
         </div>
     </nav>
-
     <!-- end navbar login -->
 @else
     <nav class="navbar navbar-expand-lg  mt-5">
@@ -85,19 +78,12 @@
                     </li>
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle text-light" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            More
+                            More Genres
                         </a>
                         <ul class="dropdown-menu multi-column">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Comedy</a></li>
-                            <li><a class="dropdown-item" href="#">Romance</a></li>
-                            <li><a class="dropdown-item" href="#">Fantasy</a></li>
-                            <li><a class="dropdown-item" href="#">Documentary</a></li>
-                            <li><a class="dropdown-item" href="#">Thriller</a></li>
-                            <li><a class="dropdown-item" href="#">Anime</a></li>
-                            <li><a class="dropdown-item" href="#">Drama</a></li>
-                            <li><a class="dropdown-item" href="#">Fiction</a></li>
-                            <li><a class="dropdown-item" href="#">Sci-Fi</a></li>
+                            @foreach ($genres as $genre)
+                                <li><a class="dropdown-item" href="/genres/{{$genre->slug}}">{{$genre->genre}}</a></li>
+                            @endforeach
                         </ul>
                     </li>
                 </ul>
