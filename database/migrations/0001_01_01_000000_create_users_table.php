bdba84c8d5b18dc978bd090f63e1dc141c18f9de
HearTh13 <gun123sokero@gmail.com>
2024-06-11 19:52:42 +0700

    modified:   database/migrations/0001_01_01_000000_create_users_table.php

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('username');
-            $table->string('password');
             $table->text('address');
             $table->string('phone_number');
             $table->string('role')->default('user');
