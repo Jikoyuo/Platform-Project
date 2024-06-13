6867353c6919c397d503a97118f7f6ca27c936a9
StefanusSaputra4 <stefanus.usd@gmail.com>
2024-06-13 15:06:42 +0700

    ya

--- config/services.php
+++ config/services.php
@@ -37,6 +37,6 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => 'http://example.com/callback-url',
+        'redirect' => 'http://127.0.0.1:8000/auth/google/call-back',
     ],
 ];
