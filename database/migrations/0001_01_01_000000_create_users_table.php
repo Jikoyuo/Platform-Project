ea3ecf86cc31a1f96b5fee2540202a6f34c54a70
HearTh13 <gun123sokero@gmail.com>
2024-06-13 15:16:18 +0700

    modified:   app/Http/Controllers/DBCartController.php 	modified:   database/migrations/0001_01_01_000000_create_users_table.php

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('google_id');
             $table->string('username');
             $table->text('address');
             $table->string('phone_number');
