419eb8b18ba8218efd11853cf44b3a3b2a7b3607
StefanusSaputra4 <stefanus.usd@gmail.com>
2024-06-13 16:37:40 +0700

    Menambahkan login google

--- config/services.php
+++ config/services.php
@@ -37,6 +37,6 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => 'http://127.0.0.1:8000/auth/google/call-back',
+        'redirect' => 'http://127.0.0.1:8000/auth/google/callback',
     ],
 ];
