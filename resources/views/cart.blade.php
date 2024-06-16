cf097f979452acf97f2d69e040afa1147eec4d2c
Jikoyuo <chornaeld@gmail.com>
2024-06-16 22:58:45 +0700

    memperbaiki profil

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -10,6 +10,10 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="../trans.css">
+    <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+            crossorigin="anonymous"></script>
 
 </head>
 
@@ -19,7 +23,7 @@
 
 
     <div class="main-content">
-        @if ($cart != null)
+        @if ($cart == null)
             <div class="trans">
                 <h1 class="text-light">Rincian Belanja</h1>
                 <h2 class="text-light">Item :</h2>
@@ -33,6 +37,7 @@
                 </div>
             </div>
         @endif
+
 
         <div class="container-trans">
             <div class="container-title"></div>
