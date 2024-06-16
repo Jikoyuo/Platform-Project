79ef2fd3bc42495baf31689b1393489180913d31
Jikoyuo <chornaeld@gmail.com>
2024-06-16 23:36:43 +0700

    menambahkan styling particle baru yang dipisahkan

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -10,6 +10,8 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="../trans.css">
+    <link rel="stylesheet" href="../newParticle.css">
+    <script defer src="../newParticle.js"></script>
     <script
             src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
@@ -20,10 +22,11 @@
 <body>
     <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
     @include('partials.navbar')
+    <div id="particles-js" class="particles"></div>
 
 
     <div class="main-content">
-        @if ($cart == null)
+        @if ($cart != null)
             <div class="trans">
                 <h1 class="text-light">Rincian Belanja</h1>
                 <h2 class="text-light">Item :</h2>
@@ -264,6 +267,8 @@
 
         })();
     </script>
+
+<script src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
 
 </body>
 
