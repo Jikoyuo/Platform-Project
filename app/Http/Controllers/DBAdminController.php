049a2f462509187af45f2be2028ff2f4c55c09d2
HearTh13 <gun123sokero@gmail.com>
2024-06-17 21:14:46 +0700

    Changes to be committed: 	modified:   app/Http/Controllers/DBAdminController.php 	modified:   app/Models/DBProduct.php 	modified:   resources/views/admin.blade.php 	modified:   routes/web.php

--- app/Http/Controllers/DBAdminController.php
+++ app/Http/Controllers/DBAdminController.php
@@ -17,4 +17,21 @@ class DBAdminController extends Controller
             'users' => $users
         ]);
     }
+
+    public function store(Request $request){
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'required|max:99999',
+            'slug' => 'required|string|max:255',
+            'year' => 'required|string|max:255',
+            'price' => 'required|numeric',
+            'stock' => 'required|integer',
+            'img_url' => 'required|max:99999',
+            'trailer' => 'required|max:99999',
+        ]);
+
+        DBProduct::create($request->all());
+
+        return redirect('/admin');
+    }
 }
