fbca502690520ae6e262062575d83865034814c8
StefanusSaputra4 <stefanus.usd@gmail.com>
2024-06-21 04:30:04 +0700

    new file:   public/BNI.webp 	new file:   public/bca.jpeg 	new file:   public/bri.jpg 	new file:   public/gopay.jpg 	modified:   public/mandiri.png 	new file:   public/ovo.png 	new file:   public/qris.jpg 	new file:   public/shoopee.jpg 	modified:   resources/views/partials/navbar.blade.php 	modified:   resources/views/payment.blade.php

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -19,10 +19,14 @@
                 <li class="nav-item">
                     <a class="nav-link text-light" href="/genres">Genres</a>
                 </li> -->
+                <li class="nav-item">
+                    <a class="nav-link active text-white" aria-current="page" href="/">Product</a>
+                </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                          Genres
                     </a>
+                    
                     <ul class="dropdown-menu multi-column">
                         @foreach ($genres as $genre)
                         <li><a class="dropdown-item" href="/genres/{{$genre->slug}}">{{$genre->genre}}</a></li>
