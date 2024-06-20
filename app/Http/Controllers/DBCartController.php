645f9b9ae6e8b6658ee34c3f463c82afa719f900
StefanusSaputra4 <stefanus.usd@gmail.com>
2024-06-20 20:03:07 +0700

    modified:   app/Http/Controllers/DBCartController.php 	modified:   resources/views/payment.blade.php

--- app/Http/Controllers/DBCartController.php
+++ app/Http/Controllers/DBCartController.php
@@ -58,7 +58,8 @@ class DBCartController extends Controller
         $request->validate([
             'price' => 'required',
             'product_id' => 'required',
-            'user_id' => 'required'
+            'user_id' => 'required',
+
         ]);
 
         DBCart::create($request->all());
